06fc83da2ea0e44c8d0de3ec51974c463f5e0781
SiddharthB70 <siddharth7002b@gmail.com>
2022-02-10 17:04:01 +0530

    Window mouseup

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 const buttons = document.querySelectorAll(".button");
+let buttondown = false;
 
+window.onload = ()=>{
+    buttons.forEach(function(button){
+        button.classList.add("noselect");
+        button.addEventListener("mousedown",buttonDown)
+        ;
+        window.addEventListener("mouseup",buttonUp);
+        /*If mouse is dragged during mousedown, mouseup 
+        and click aren't triggered. To prevent that, 
+        mouseup is instead used on window*/
+    });
+}
 
-//Button ClicK
-buttons.forEach(function(button){
-    button.addEventListener("click",buttonClick);
-});
+function buttonDown(e){
+    buttonDown = true;
+    toggleButton(e.target);
+}
 
-function buttonClick(e){
-    let button = e.target;
-    toggleButton(button);
-    setTimeout(function(button){
-        toggleButton(button);
-    },200,button);
+function buttonUp(){
+    if(buttonDown){
+        buttonDown = false;
+        toggleButton(document.querySelector(".click"));
+    }
 }
 
 function toggleButton(button){
     button.classList.toggle("click");
 }
+
 
