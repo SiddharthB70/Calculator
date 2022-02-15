399ecc863862cea9bd87dd954c1e7545390da338
SiddharthB70 <siddharth7002b@gmail.com>
2022-02-15 20:02:01 +0530

    Basic Operations and Displaying

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const lowerDisplay = document.getElementById("lower");
 
 let buttondown = false;//flag
 let displayDefault = true; 
+let lowerDisplayValues = [];
+let operatorEncountered = false;
+let mainExp = new Expression();
 
 window.onload = ()=>{
     lowerDisplay.textContent = 0;
@@ -17,10 +20,19 @@ window.onload = ()=>{
     });
 }
 
+function Expression(){
+    this.value1 = "";
+    this.operator = "";
+    this.value2 = "";
+    this.opInputed = false;
+    this.expInputed = false;
+}
+
 function buttonDown(e){
     buttondown = true;
     toggleButton(e.target);
-    pushIntoDisplay(e.target.getAttribute("data-value"));
+    // getValue(e.target.getAttribute("data-value"));
+    storeValue(e.target.getAttribute("data-value"));
 }
 
 function buttonUp(){
@@ -34,16 +46,86 @@ function toggleButton(button){
     button.classList.toggle("click");
 }
 
-function pushIntoDisplay(value){
+function getValue(value){
     if(value === "AC"){
         displayDefault = true;
         lowerDisplay.textContent = 0;
+        operatorEncountered = false
         return;
     }
-    if(displayDefault){
-        lowerDisplay.textContent = value;
-        displayDefault = false;
+    else if(/[0-9]/.test(value)){
+        if(displayDefault)
+            lowerDisplay.textContent = "";
+        lowerDisplay.textContent += value;
+        operatorEncountered = false;
+        
+    }
+    else if(/[*+-/]/.test(value)){
+        if(operatorEncountered)
+            return;
+        lowerDisplay.textContent += value;
+        operatorEncountered = true;
+
+    }
+    displayDefault = false;
+}
+
+function storeValue(value){
+    if(value === "AC"){
+        allClear();
         return;
-    } 
-    lowerDisplay.textContent += value;
+    }
+    else if(/[0-9]/.test(value)){
+        inputDigit(value);
+    }
+    else if(/[*+-/]/.test(value)){
+        inputOperator(value);
+    }
+    console.log(mainExp);
+    lowerDisplay.textContent = mainExp.value1 + mainExp.operator + mainExp.value2;
+}
+
+function allClear(){
+    mainExp.value1 = "";
+    mainExp.operator = "";
+    mainExp.value2 = "";
+    mainExp.opInputed = false;
+    mainExp.expInputed = false;
+    lowerDisplay.textContent = "0";
+}
+
+function inputDigit(digit){
+    if(!mainExp.opInputed)
+        mainExp.value1 = ((mainExp.value1*10) + Number(digit));
+    else    
+        {mainExp.value2 = ((mainExp.value2*10) + Number(digit));
+            mainExp.expInputed = true;
+        }
+    if(mainExp.opInput)
+        mainExp.opInputed = false;
+}
+
+function inputOperator(operator){
+    if(mainExp.opInputed){
+        if(!mainExp.expInputed)
+            return;
+        else
+            operate();
+    }
+    mainExp.operator = operator;
+    mainExp.opInputed = true;
+}
+
+function operate(){
+    switch(mainExp.operator){
+        case "+":   mainExp.value1 += mainExp.value2;
+                    break;
+        case "-":   mainExp.value1 -= mainExp.value2;
+                    break;
+        case "*":   mainExp.value1 *= mainExp.value2;
+                    break;
+        case "/":   mainExp.value1 /= mainExp.value2;
+                    break;      
+    }
+    mainExp.value2 = "";
 }
